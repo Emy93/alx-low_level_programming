0b46615be4d4e71eb3a3c9214e2b31351e2c544f
Emy93 <imen.khe3@gmail.com>
2022-06-30 17:50:41 -0700

    C programming tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 /**
-*main - Entry point 
-*Return: Always 0 (success)
+*main - Entry point
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
- 
-i
+printf("Programming is like building a multilingual puzzle,\n");
+return (0);
 }
